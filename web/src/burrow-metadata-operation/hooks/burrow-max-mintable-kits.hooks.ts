9132906d7ae493b84b3ee966f24f0d36a84d7650
Valentino Drappa <valentino@Valentinos-MacBook-Pro.local>
2021-06-25 01:21:37 +0200

    implement burrow-max-mintable-kits

---
+++ web/src/burrow-metadata-operation/hooks/burrow-max-mintable-kits.hooks.ts
@@ -0,0 +1,60 @@
+import { getContract, RequestStatus, TzFormatMutezToTz } from '@config'
+import { useGetWallet } from '@wallet'
+import BigNumber from 'bignumber.js'
+import { useEffect, useState } from 'react'
+
+const zero = new BigNumber(0)
+type Data = {
+  maxMintableKits: BigNumber
+  status: RequestStatus
+}
+
+export const useBurrowMaxMintableKits = (
+  checkerScAdress: string,
+  burrowId: number,
+): [Data, () => void] => {
+  const { address: walletAdress } = useGetWallet()
+
+  const [data, setData] = useState({
+    maxMintableKits: zero,
+    status: RequestStatus.pending,
+  })
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // we want the load the data only when data.status === pending
+      if (data.status !== RequestStatus.pending) {
+        return
+      }
+
+      try {
+        const contract = await getContract(checkerScAdress)
+
+        // eslint-disable-next-line
+        // @ts-ignore
+        const metadataViews = await contract.tzip16().metadataViews()
+
+        const maxMintableKits = TzFormatMutezToTz(
+          await metadataViews.burrow_max_mintable_kit().executeView(walletAdress, burrowId),
+        )
+
+        setData({ maxMintableKits, status: RequestStatus.success })
+      } catch (error) {
+        setData({ maxMintableKits: zero, status: RequestStatus.error })
+      }
+    }
+
+    fetchData()
+  }, [checkerScAdress, burrowId, setData, data])
+
+  // reload is accessible only if status === error
+  const reload = () =>
+    data.status === RequestStatus.error
+      ? setData({
+          maxMintableKits: zero,
+          status: RequestStatus.pending,
+        })
+      : null
+
+  return [{ ...data }, reload]
+}
