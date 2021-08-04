e24667ff5dbda2cadfae1eb71c6255a08eb7e305
Valentino Drappa <drappav@gmail.com>
2021-08-04 18:19:00 +0200

    rename meta-view-...hook.ts -> meta-view-...hooks.ts

--- web/src/meta-view-operation/index.ts
+++ web/src/meta-view-operation/index.ts
@@ -1,4 +1,4 @@
-export { useMetaViewBuyKitMinKitExpected } from './hooks/meta-view-buy-kit-min-kit-expected.hook'
+export { useMetaViewBuyKitMinKitExpected } from './hooks/meta-view-buy-kit-min-kit-expected.hooks'
 export { useMetaViewMaxMintableKits } from './hooks/meta-view-max-mintable-kits.hooks'
-export { useMetaViewSellKitMinCtezExpected } from './hooks/meta-view-sell-kit-min-ctez-expected.hook'
-export { useMetaViewTokenMetaData } from './hooks/meta-view-token-meda-data.hook'
+export { useMetaViewSellKitMinCtezExpected } from './hooks/meta-view-sell-kit-min-ctez-expected.hooks'
+export { useMetaViewTokenMetaData } from './hooks/meta-view-token-meda-data.hooks'
