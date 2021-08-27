56a2d7e8af2d8813874f5910633f4b31f5f7b3a9
Valentino Drappa <drappav@gmail.com>
2021-08-27 23:21:37 +0200

    add cfmm responsive rules

--- web/src/pages/cfmm-page.tsx
+++ web/src/pages/cfmm-page.tsx
@@ -7,7 +7,7 @@ export const CfmmPage: FunctionComponent = () => {
   const { isOpen, onToggle } = useDisclosure()
 
   return (
-    <Box display="flex" flexDirection="column" height="85vh" w="90vw">
+    <Box display="flex" flexDirection="column" height="85vh" w={['95vw', '95vw', '90vw']}>
       <Box display="flex" justifyContent="space-between" alignItems="center" p="2">
         <Box fontSize="2xl">Cfmm</Box>
       </Box>
