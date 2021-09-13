738f643d36ccf46aee692b1255d5233e98f005ea
IronCash <vaneeckhoutnicolas@gmail.com>
2021-09-13 18:55:42 +0200

    feat(PageInfo): new component for metrics desktop and mobile extend

---
+++ web/src/pages/common/page-info.tsx
@@ -0,0 +1,6 @@
+import { Flex } from '@chakra-ui/react'
+import React from 'react'
+
+export const PageInfo: React.FC = () => (
+  <Flex bg={'blue.500'} p={['5px', '5px', '10px']} justifyContent={'space-between'}></Flex>
+)
