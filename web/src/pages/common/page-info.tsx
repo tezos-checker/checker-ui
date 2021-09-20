4a20cd0bd37dd961b40c97316a71cf3877f152bd
IronCash <vaneeckhoutnicolas@gmail.com>
2021-09-20 15:55:36 +0200

    refacto(metrics): burrowid hack : 0

--- web/src/pages/common/page-info.tsx
+++ web/src/pages/common/page-info.tsx
@@ -1,6 +1,6 @@
 import { Flex } from '@chakra-ui/react'
 import { RequestStatus } from '@config'
-import { useStorageDispatcher } from '@storage'
+import { useGetStorage, useStorageDispatcher } from '@storage'
 import React, { useEffect } from 'react'
 import {
   EmptyBurrowStorage,
@@ -9,6 +9,7 @@ import {
 
 export const PageInfo: React.FC = () => {
   const { loadCheckerStorage } = useStorageDispatcher()
+  const storage = useGetStorage(0) // HACK BURROW ID = 0 as only CHECKER global metrics
 
   //  refactoring o, burrow part later as useless
   useEffect(() => {
@@ -21,5 +22,13 @@ export const PageInfo: React.FC = () => {
     })
   }, [])
 
-  return <Flex bg={'blue.500'} p={['5px', '5px', '10px']} justifyContent={'space-between'}></Flex>
+  // new_target = new_q * (new_index / kit_in_tez_now)
+
+  return (
+    <Flex bg={'blue.500'} p={['5px', '5px', '10px']} justifyContent={'space-between'}>
+      Last Update : {storage?.checkerStorage.last_touched}
+      Oracle Price : {storage?.checkerStorage.index}
+      Target Price : 1 Drift : {storage?.checkerStorage.drift}
+    </Flex>
+  )
 }
